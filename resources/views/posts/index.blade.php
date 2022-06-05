7e4fac86d2855a381965df680b752ca7d5d9f2f9
wasao123 <jobhunting0414@gmail.com>
2022-06-05 15:08:11 +0900

    CSSを適応、変更できるようにしました。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        <link rel="stylesheet" href="{{asset('/css/index.css')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
