42cd2c0a8d426fc5081736d576ba893e7c263c76
urabetakaharu <ut1180013@gmail.com>
2022-06-05 15:43:45 +0900

    cssのjsの更新

--- database/seeds/KasaTableSeeder.php
+++ database/seeds/KasaTableSeeder.php
@@ -19,6 +19,44 @@ class KasaTableSeeder extends Seeder
 		'count'=> 0,
 		'created_at' => now(),
         'updated_at' => now(),
-		]);
+		],
+		[
+		  'id'=>2, 
+		'name'=> '小川町駅',
+		'lat'=>35.69496,
+		'lng'=> 139.76746000000003,
+		'count'=> 0,
+		'created_at' => now(),
+        'updated_at' => now(),   
+		],
+		[
+		  'id'=>3, 
+		'name'=> '御茶ノ水駅',
+		'lat'=>35.6993529,
+		'lng'=> 139.76526949999993,
+		'count'=> 8,
+		'created_at' => now(),
+        'updated_at' => now(),   
+		],
+		[
+		  'id'=>4, 
+		'name'=> '神保町駅',
+		'lat'=>35.695932,
+		'lng'=> 139.75762699999996,
+		'count'=> 4,
+		'created_at' => now(),
+        'updated_at' => now(),   
+		],
+		[
+		  'id'=>5, 
+		'name'=> '新御茶ノ水駅',
+		'lat'=>35.696932,
+		'lng'=> 139.76543200000003,
+		'count'=> 1,
+		'created_at' => now(),
+        'updated_at' => now(),   
+		],
+		
+		);
     }
 }
